ad3d1d6ad77362296c33f7b1387e68edd041904f
Zijuan Zhao <rq020616@mail.ustc.edu.cn>
2026-06-03 18:38:47 +0800

    Update model path and output file in eval script

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=3 python src/eval/test_value.py \
     --json-file Benchmark.jsonl \
     --model-base Qwen3-VL-2B-Instruct \
-    --model-path output/qwen3vl_2b_dpo_largebatch_4epoch_balance \
+    --model-path output/qwen3vl_2b_samdpo \
     --device cuda \
     --disable_flash_attention \
-    --output-file eval_output/dpo_lora_3_2b_4epoch_Lbatch_balance.jsonl
+    --output-file eval_output/qwen3vl_2b_samdpo.jsonl
     
